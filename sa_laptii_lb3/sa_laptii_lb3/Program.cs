d683258361df5881bf8e36f5496929f52744b50c
Anastasiia Laptii <anastasiia.laptii@tech-stack.io>
2022-07-11 12:29:30 +0300

    init

---
+++ sa_laptii_lb3/sa_laptii_lb3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sa_laptii_lb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scanner scanner = new Scanner("H:\\university\\магистратура кек ты до нее дожила\\war semester\\вечур\\lb3\\CocoR-CSharp\\src\\Input.txt");
+            Parser parser = new Parser(scanner);
+            parser.Parse();
+            Console.Write(parser.errors.count + " errors detected");
+
+            Console.ReadKey();
+        }
+    }
+}
